e91c3995f12be082b4057e8c3516709f0f2ba285
Natalia Katsiapi <kat26natalia@gmail.com>
2020-12-24 15:29:07 +0200

    Make progress on model

--- Solver.py
+++ Solver.py
@@ -1,3 +1,20 @@
 import random
 from VRP_Model import *
 
+class Solution:
+    def __init__(self):
+        # το μέγιστο όλων των routes που αποτελεί τον ελάχιστο χρόνο ολοκλήρωσης της παραλαβής των προϊόντων
+        self.time_cost = 0
+        self.routes = []
+
+class Solver:
+    def __init__(self, m):
+        self.all_nodes = m.all_nodes
+        self.service_locations = m.service_locations
+        self.depot = m.all_nodes[0]
+        self.time_matrix = m.time_matrix
+        self.capacity = m.capacity
+        self.sol = None
+        # self.bestSolution = None
+        # self.overallBestSol = None
+        self.rcl_size = 3 #isws gia arxh na to kaname 1
--- VRP_Model.py
+++ VRP_Model.py
@@ -1,19 +1,23 @@
 import random
 import math
 
-class Node:
-    def __init__(self, id, tp, dem, xx, yy):
-        self.id = id
-        self.type = tp
-        self.demand = dem
-        self.x = xx
-        self.y = yy
+class Model:
+
+# instance variables
+    def __init__(self):
+        self.all_nodes = []
+        self.service_locations = []
+        self.time_matrix = []
+        self.capacity = -1
+        self.total_vehicles = -1
 
     def build_model(self):
-        all_nodes = []
-        service_locations = []
+        self.all_nodes = []
+        self.service_locations = []
         depot = Node(0, 0, 0, 50, 50)
-        all_nodes.append(depot)
+        self.capacity = 3000
+        self.total_vehicles = 25
+        self.all_nodes.append(depot)
         random.seed(1)
 
         for i in range(0, 200):
@@ -23,25 +27,46 @@ class Node:
             xx = random.randint(0, 100)
             yy = random.randint(0, 100)
             serv_node = Node(id, tp, dem, xx, yy)
-            all_nodes.append(serv_node)
-            service_locations.append(serv_node)
+            self.all_nodes.append(serv_node)
+            self.service_locations.append(serv_node)
 
-        dist_matrix = [[0.0 for j in range(0, len(all_nodes))] for k in range(0, len(all_nodes))]
-        for i in range(0, len(all_nodes)):
-            for j in range(0, len(all_nodes)):
-                source = all_nodes[i]
-            target = all_nodes[j]
-            dx_2 = (source.x - target.x) ** 2
-            dy_2 = (source.y - target.y) ** 2
-            dist = round(math.sqrt(dx_2 + dy_2))
-            dist_matrix[i][j] = dist
+        self.time_matrix = [[0.0 for j in range(0, len(self.all_nodes))] for k in range(0, len(self.all_nodes))]
+        for i in range(0, len(self.all_nodes)):
+            for j in range(0, len(self.all_nodes)):
+                source = self.all_nodes[i]
+                target = self.all_nodes[j]
+                dx_2 = (source.x - target.x) ** 2
+                dy_2 = (source.y - target.y) ** 2
+                dist = round(math.sqrt(dx_2 + dy_2))
+                time = dist / 35
+                if source.type == 0:
+                    continue
+                elif source.type == 1:
+                    time += 1 / 12
+                elif source.type == 2:
+                    time += 1 / 4
+                else:
+                    time += 5 / 12
+                self.time_matrix[i][j] = time
 
-        dist_matrix = [[0.0 for j in range(0, len(all_nodes))] for k in range(0, len(all_nodes))]
-        for i in range(0, len(all_nodes)):
-            for j in range(0, len(all_nodes)):
-                source = all_nodes[i]
-            target = all_nodes[j]
-            dx_2 = (source.x - target.x) ** 2
-            dy_2 = (source.y - target.y) ** 2
-            dist = round(math.sqrt(dx_2 + dy_2))
-            dist_matrix[i][j] = dist
+
+class Node:
+    def __init__(self, id, tp, dem, xx, yy):
+        self.id = id
+        self.type = tp
+        self.demand = dem
+        self.x = xx
+        self.y = yy
+        self.isRouted = False
+
+class Route:
+    def __init__(self, dp, cap):
+        self.sequenceOfNodes = []
+        self.sequenceOfNodes.append(dp)
+        # self.sequenceOfNodes.append(dp)
+        self.time = 0
+        self.capacity = cap
+        self.load = 0
+
+
+

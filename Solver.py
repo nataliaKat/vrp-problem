0b31f0e55a3142304e0ac21d283cb5d648ed09a4
Natalia Katsiapi <kat26natalia@gmail.com>
2020-12-31 13:00:20 +0200

    Remove 1 tab

--- Solver.py
+++ Solver.py
@@ -41,7 +41,7 @@ class Solver:
         # for r in self.sol.routes:
         #     r.printRoute()
         # SolDrawer.draw(1, self.sol, self.all_nodes)
-        self.testSolution("sol.txt")
+        self.testSolution("sol-25")
 
     def setRoutedFlagToFalseForAllServiceLocations(self):
         for i in range(0, len(self.service_locations)):
@@ -167,88 +167,88 @@ class Solver:
                 # reportSolution
 
 
-        def identifyBestInsertionAllPositions(self, bestInsertion, rt, itr = 0):
-            random.seed(itr)
-            rcl = []
-            for i in range(0, len(self.service_locations)):
-                candidateServLoc: Node = self.service_locations[i]
-                if candidateServLoc.isRouted is False:
-                    if rt.load + candidateServLoc.demand <= rt.capacity:
-                        if len(rt.sequenceOfNodes)==1:
-                            trialTime=self.time_matrix[0][candidateServLoc.id]
-                            bestInsertion.time = trialTime
-                            bestInsertion.serviceLocation = candidateServLoc
-                            bestInsertion.route = rt
-                            bestInsertion.insertionPosition = 1
-                            return
-                        for j in range(0, len(rt.sequenceOfNodes)-1):
-                            A = rt.sequenceOfNodes[j]
-                            B = rt.sequenceOfNodes[j + 1]
-                            timeAdded = self.time_matrix[A.id][candidateServLoc.id] + self.time_matrix[candidateServLoc.id][B.id]
-                            timeRemoved = self.time_matrix[A.id][B.id]
-                            trialTime = timeAdded - timeRemoved
+    def identifyBestInsertionAllPositions(self, bestInsertion, rt, itr = 0):
+        random.seed(itr)
+        rcl = []
+        for i in range(0, len(self.service_locations)):
+            candidateServLoc: Node = self.service_locations[i]
+            if candidateServLoc.isRouted is False:
+                if rt.load + candidateServLoc.demand <= rt.capacity:
+                    if len(rt.sequenceOfNodes)==1:
+                        trialTime=self.time_matrix[0][candidateServLoc.id]
+                        bestInsertion.time = trialTime
+                        bestInsertion.serviceLocation = candidateServLoc
+                        bestInsertion.route = rt
+                        bestInsertion.insertionPosition = 1
+                        return
+                    for j in range(0, len(rt.sequenceOfNodes)-1):
+                        A = rt.sequenceOfNodes[j]
+                        B = rt.sequenceOfNodes[j + 1]
+                        timeAdded = self.time_matrix[A.id][candidateServLoc.id] + self.time_matrix[candidateServLoc.id][B.id]
+                        timeRemoved = self.time_matrix[A.id][B.id]
+                        trialTime = timeAdded - timeRemoved
 
-                            if len(rcl) < self.rcl_size:
-                                new_tup = (trialTime, candidateServLoc, rt, j)
-                                rcl.append(new_tup)
-                                rcl.sort(key=lambda x: x[0])
-                            elif trialTime < rcl[-1][0]:
-                                rcl.pop(len(rcl) - 1)
-                                new_tup = (trialTime, candidateServLoc, rt, j)
-                                rcl.append(new_tup)
-                                rcl.sort(key=lambda x: x[0])
-            if len(rcl) > 0:
-                tup_index = random.randint(0, len(rcl)-1)
-                tpl = rcl[tup_index]
-                bestInsertion.time = tpl[0]
-                bestInsertion.serviceLocation = tpl[1]
-                bestInsertion.route = tpl[2]
-                bestInsertion.insertionPosition = tpl[3]
+                        if len(rcl) < self.rcl_size:
+                            new_tup = (trialTime, candidateServLoc, rt, j)
+                            rcl.append(new_tup)
+                            rcl.sort(key=lambda x: x[0])
+                        elif trialTime < rcl[-1][0]:
+                            rcl.pop(len(rcl) - 1)
+                            new_tup = (trialTime, candidateServLoc, rt, j)
+                            rcl.append(new_tup)
+                            rcl.sort(key=lambda x: x[0])
+        if len(rcl) > 0:
+            tup_index = random.randint(0, len(rcl)-1)
+            tpl = rcl[tup_index]
+            bestInsertion.time = tpl[0]
+            bestInsertion.serviceLocation = tpl[1]
+            bestInsertion.route = tpl[2]
+            bestInsertion.insertionPosition = tpl[3]
 
-        def applyLocationInsertionAllPositions(self, insertion):
-            insLocation = insertion.serviceLocation
-            rt = insertion.route
-            # before the second depot occurrence
-            insIndex = insertion.insertionPosition
-            rt.sequenceOfNodes.insert(insIndex + 1, insLocation)
-            rt.time += insertion.time
-            routes_sorted = self.sol.routes[:]
-            routes_sorted.sort(key=lambda x: x.time)
-            self.sol.time_cost = routes_sorted[-1].time
-            rt.load += insLocation.demand
-            insLocation.isRouted = True
-            
-        def testSolution(self, filename):
-            f = open(filename)
-            lines = f.read().splitlines()
-            f.close()
-            file_obj = -1
-            file_sol = Solution()
-            depot = Node(0, 0, 0, 50, 50)
-            for i in range(len(lines)):
-                if i == 0:
-                    file_sol.time_cost = float(lines[i])
-                else:
-                    rt = Route(depot, 3000)
-                    file_sol.routes.append(rt)
-                    ids = lines[i].split(",")
-                    for i in range(len(ids)):
-                        if i != 0:#gia na min valei tin apothiki
-                            id = int(ids[i])
-                            rt.sequenceOfNodes.append(self.all_nodes[id])
-                    rt.printRoute()
-            for route in file_sol.routes:
-                route.time = 0
-                for i in range(len(route.sequenceOfNodes) - 1):
-                    current = route.sequenceOfNodes[i]
-                    print("current", current.id)
-                    next = route.sequenceOfNodes[i + 1]
-                    print("next", next.id)
-                    route.time += self.time_matrix[current.id][next.id]
+    def applyLocationInsertionAllPositions(self, insertion):
+        insLocation = insertion.serviceLocation
+        rt = insertion.route
+        # before the second depot occurrence
+        insIndex = insertion.insertionPosition
+        rt.sequenceOfNodes.insert(insIndex + 1, insLocation)
+        rt.time += insertion.time
+        routes_sorted = self.sol.routes[:]
+        routes_sorted.sort(key=lambda x: x.time)
+        self.sol.time_cost = routes_sorted[-1].time
+        rt.load += insLocation.demand
+        insLocation.isRouted = True
 
-            routes_sorted = file_sol.routes[:]
-            routes_sorted.sort(key=lambda x: x.time)
-            print("---------")
-            print(routes_sorted)
-            file_sol.time_cost = routes_sorted[-1].time
-            print("Time is", file_sol.time_cost)
+    def testSolution(self, filename):
+        f = open(filename)
+        lines = f.read().splitlines()
+        f.close()
+        file_obj = -1
+        file_sol = Solution()
+        depot = Node(0, 0, 0, 50, 50)
+        for i in range(len(lines)):
+            if i == 0:
+                file_sol.time_cost = float(lines[i])
+            else:
+                rt = Route(depot, 3000)
+                file_sol.routes.append(rt)
+                ids = lines[i].split(",")
+                for i in range(len(ids)):
+                    if i != 0:#gia na min valei tin apothiki
+                        id = int(ids[i])
+                        rt.sequenceOfNodes.append(self.all_nodes[id])
+                rt.printRoute()
+        for route in file_sol.routes:
+            route.time = 0
+            for i in range(len(route.sequenceOfNodes) - 1):
+                current = route.sequenceOfNodes[i]
+                print("current", current.id)
+                next = route.sequenceOfNodes[i + 1]
+                print("next", next.id)
+                route.time += self.time_matrix[current.id][next.id]
+
+        routes_sorted = file_sol.routes[:]
+        routes_sorted.sort(key=lambda x: x.time)
+        print("---------")
+        print(routes_sorted)
+        file_sol.time_cost = routes_sorted[-1].time
+        print("Time is", file_sol.time_cost)
